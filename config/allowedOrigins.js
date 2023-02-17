5d061f68d4570b3a4daff1103cd6099a80d302e4
ozgur <ozgurcokckn@gmail.com>
2023-02-17 23:00:38 +0300

    giving cors path

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ["http://localhost:3000", "http://127.0.0.1:5173"];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://127.0.0.1:5173",
+  "https://dufren-ecommerce-app.onrender.com",
+];
 
 module.exports = allowedOrigins;
